c6de79b6f8978a64ca88935bdd83bded9a80c0b0
warwick <warwick.nortier@gmail.com>
2022-05-14 21:04:51 +0200

    edited db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "b551a660f9334d",
+    PASSWORD: "b615779a",
+    DB: "heroku_97919d5c7f46217"
 };
